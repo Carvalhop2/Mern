3124e8bb23a4d886fb7aa3c82d3fdcf9ef7303a5
Carvalhop2 <wellingtonp2.oc@gmail.com>
2025-12-10 10:15:00 -0300

    conf. server1

---
+++ beckend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import "dotenv/config.js";
+import mongoose from "mongoose";
+
+
+const app = express();
+async function connectToDatabase() {
+   try {
+    await mongoose.connect(MONGO_URL);
+    console.log("Connected to MongoDB");
+} catch (error) {
+    console.error("Error connecting to MongoDB database:", error);
+    process.exit(1);
+}
+
+
+app.listen (3000, () => {
+    console.log('Server is running on port 3000. CLTR+C to stop. Eita Deus');
+    connectToDatabase();
+});
+
+}
+
+// MongoDB. User: wellingtonp2oc_db_user. Senha:MDB: B2MVoIvhd1dQQcZR
