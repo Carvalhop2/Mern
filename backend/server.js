be61361447e4639ff1ce76ffb38e3f18bb743b81
Carvalhop2 <wellingtonp2.oc@gmail.com>
2025-12-12 19:45:44 -0300

    crud

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,29 @@
 import express from 'express';
 import "dotenv/config.js";
-import mongoose from "mongoose";
-import {Book} from './models/Book.js';
-
+import { connectToDatabase } from './db/connect.js';
+import { Book } from './model/book.model.js';
 
 const app = express();
 
-export async function connectToDatabase() {
-   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDB");
-} catch (error) {
-    console.error("Error connecting to MongoDB database:", error);
-    process.exit(1);
-}
+app.post("/api/v1/books", async (req, res) => {
+    const { title, subtitle, author, genre, cover } = req.body;
+    try{
+        const book = new Book({title, subtitle, author, genre, cover});
+        await book.save();
+        res.status(201).json({sucess: true, data: book });
+    }
+    catch (error) {
+        console.error("Error saving book:", error);
+        res.status(500).json({sucess: false, message: "Erro durante o cadastro"});        
+    }
 
+});
 
 app.listen (3000, () => {
     console.log('Server is running on port 3000. CLTR+C to stop. Eita Deus');
     connectToDatabase();
 });
 
-}
+
 
 // MongoDB. User: wellingtonp2oc_db_user. Senha:MDB: B2MVoIvhd1dQQcZR
