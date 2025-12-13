5a1b08c3a3d2d08f41e48d63eb32dd9890513df9
Carvalhop2 <wellingtonp2.oc@gmail.com>
2025-12-13 16:33:02 -0300

    com alterações

--- backend/model/book.model.js
+++ backend/model/book.model.js
@@ -1,28 +1,27 @@
 import mongoose from "mongoose";
+
 const bookSchema = new mongoose.Schema({
-    title: {
-        type: String, require: true
+    title:{
+        type:String,
+        require:true,
     },
-    subtitle: {
-        type: String,
-        require: true
+    subtitle:{
+        type:String,
+        require:true,
     },
-    author: {
-        type: String, require: true
+    author:{
+        type:String,
+        require:true,
     },
-    genre: {
-        type: String, require: true
+    genre:{
+        type:String,
+        require:true,
     },
     cover:{
-        type: String, 
-        require: true
-    }
-},
-
-{timestamps: true}
-
-);
-
-
+        type:String,
+        require:true,
+    },    
+}, 
+{timestamps:true});
 
 export const Book = mongoose.model("Book", bookSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,26 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config.js";
-import { connectToDatabase } from './db/connect.js';
-import { Book } from './model/book.model.js';
-
+import { connectToDatabase } from "./db/connect.js";
+import { Book } from "./model/book.model.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/api/v1/books", async (req, res) => {
-    
-    const { title, subtitle, author, genre, cover } = req.body;
-    
+app.post("/api/v1/books", async (req,res)=>{
+    const{ title,subtitle,author,genre,cover }=req.body;
     try{
-        const book = new Book({title, subtitle, author, genre, cover});
-        await book.save();
-        res.status(201).json({sucess: true, data: book });
+        const book = new Book({title, subtitle,author, genre, cover});
+        await book.save(); 
+        res.status(201).json({sucess:true, data:book});       
+    }catch(error){
+        console.error("Error saving book",error);
+        res.status(500).json({sucess:false, error:"Erro durante cadastro."});
     }
-    catch (error) {
-        console.error("Error saving book:", error);
-        res.status(500).json({sucess: false, message: "Erro durante o cadastro"});        
-    }
-
 });
 
-    app.get("/api/v1/books", async (req, res) => {
-    try {
-        const books = await Book.find();
-        res.status(200).json({sucess: true, data: books}); 
-    } catch (error) {
-        console.error("Erro na busca por livos:", error);
-        res.status(500).json({sucess: false, erro: "Erro ao buscar por livro"});
-    }     
-}); 
-
-app.listen (3000, () => {
-    console.log('Server is running on port 3000. CLTR+C to stop. Teste do Servidor');
+app.listen (3000, ()=>{
+    console.log("Server is running on port 3000. CTRL + C to stop. Testenado a conexão com o banco de dados...");
     connectToDatabase();
 });
 
